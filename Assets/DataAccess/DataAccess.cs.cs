30fa7dd4e60aba79d722d4e98368fc103946bc57
Xenon Harpe <759005@ACDM.DS.SAIT.CA>
2020-11-06 15:47:10 -0700

    Began implementing Db connection
    This implementation is not complete. Verify MongoDB is installed properly (ask xenon for help if needed) before working on feature

---
+++ Assets/DataAccess/DataAccess.cs.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataAccess.cs : MonoBehaviour
+{
+    private const string MONGO_URI = "mongodb://username:password@127.0.0.1:27017";
+private const string DATABASE_NAME = "testDatabase";
+private MongoClient client;
+private IMongoDatabase db;
+
+client = new MongoClient(MONGO_URI);
+db = client.GetDatabase(DATABASE_NAME);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
