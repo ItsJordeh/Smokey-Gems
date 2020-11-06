f15760df1ed339eff6df8e49bd128d749317cbaa
ItsJordeh <ItsJordeh@gmail.com>
2020-11-05 18:11:03 -0700

    Created Jordans test level and implemented a CameraZoomController
    See Assets/Scenes/JordanTestLevel for demonstration on how to use Camera zoom in your test levels

---
+++ Assets/Scripts/CameraZoomController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomController : MonoBehaviour
+{
+    private Camera cam;
+    private float targetZoom;
+    private float zoomFactor = 3f;
+    private float zoomLerpSpeed = 10;
+    void Start()
+    {
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        targetZoom = targetZoom - scrollData * zoomFactor;
+        if(targetZoom > 12)
+        {
+            targetZoom = 12;
+        }
+        else if(targetZoom < 5)
+        {
+            targetZoom = 5;
+
+        }
+
+
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+
+    }
+}
