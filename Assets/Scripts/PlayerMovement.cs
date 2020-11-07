333c8923767ae043906e9f3a306393ca01070a75
ItsJordeh <ItsJordeh@gmail.com>
2020-11-07 15:47:01 -0700

    Replaced placeholder animations and added walk animations. Also added a dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,9 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     //Animator here
     public Animator animator;
-
+    public float dashSpeed;
+    public float dashCooldown;
+    public float timeStamp = 0;
     public bool isSprinting;
 
     Vector2 movement;
@@ -43,8 +45,22 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift))//Reads the shift key and runs if its being pressed, Keycode shift can be changed to a variable that we can use to allow button remapping easily
         {
             //set animator bool sprinting to true
-            isSprinting = true;
+            
+            
             animator.SetBool("isSprinting", true);
+            
+            if(timeStamp <= Time.time)
+            {
+                
+                isSprinting = true;
+            }
+            else
+            {
+                isSprinting = false;
+            }
+
+            
+            
         }
         else
         {
@@ -59,14 +75,25 @@ public class PlayerMovement : MonoBehaviour
 
         
 
-
+        
 }
     //will run actual movement of the player NOTE: runs every frame 
+    
     private void FixedUpdate()
     {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (Vector2)((mousePos - transform.position));
+        direction.Normalize();
         if (isSprinting)
         {
-            moveSpeed = 5f;
+            
+            
+                
+                this.GetComponentInParent<Rigidbody2D>().position = this.GetComponentInParent<Rigidbody2D>().position + direction * dashSpeed;
+                timeStamp = Time.time + dashCooldown;
+
+
+            //moveSpeed = 5f;
         }
         else
         {
