85d3aca1b48dd0a124cd75b7ff3cba8ec5fe73ba
Xenon Harpe <759005@ACDM.DS.SAIT.CA>
2020-11-09 21:51:28 -0700

    Tapout on db connection

--- Assets/DataAccess/DataAccess.cs
+++ Assets/DataAccess/DataAccess.cs
@@ -3,30 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataAccess.cs : MonoBehaviour
-{
-  private const string MONGO_URI = "mongodb://username:password@127.0.0.1:27017";
-  // private MongoClient client;
-  private IMongoDatabase db;
+// public class DataAccess : MonoBehaviour
+// {
+//   private const string MONGO_URI = "mongodb://username:password@127.0.0.1:27017";
+//   private MongoClient client = new MongoClient(MONGO_URI);
+//   private IMongoDatabase db;
+//   private IMongoCollection<BsonDocument> collection;
 
 
-    Start is called before the first frame update
-   void Start()
-   {
+//     // Start is called before the first frame update
+//    void Start()
+//    {
 
-  const string DATABASE_NAME = "accounts";
+//   const string DATABASE_NAME = "smokey_gems";
 
-    // client = new MongoClient(MONGO_URI);
-    db = client.GetDatabase(DATABASE_NAME);
-    collection = db.GetCollection<BsonDocument>("accounts");
+//     // client = new MongoClient(MONGO_URI);
+//     db = client.GetDatabase(DATABASE_NAME);
+//     collection = db.GetCollection<BsonDocument>("accounts");
 
-    // collection.find({});
-    debug.log(collection.find({}));
-   }
+//     // collection.find({});
+//     // Debug.log(collection.find({}));
 
-    Update is called once per frame
-    void Update()
-    {
+//     // BsonDocument document = new BsonDocument{{"username":"Xenon","password":"haha","email":"xenon@no.no","purchases":"" }};
+//     collection.InsertOne(document);
+//    }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
