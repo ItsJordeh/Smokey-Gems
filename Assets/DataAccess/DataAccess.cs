04a1880027e6a2007ed56cc6d053448688ff59e7
Xenon Harpe <759005@ACDM.DS.SAIT.CA>
2020-11-09 21:24:20 -0700

    Implemented basic db connection

---
+++ Assets/DataAccess/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataAccess.cs : MonoBehaviour
+{
+  private const string MONGO_URI = "mongodb://username:password@127.0.0.1:27017";
+  // private MongoClient client;
+  private IMongoDatabase db;
+
+
+    Start is called before the first frame update
+   void Start()
+   {
+
+  const string DATABASE_NAME = "accounts";
+
+    // client = new MongoClient(MONGO_URI);
+    db = client.GetDatabase(DATABASE_NAME);
+    collection = db.GetCollection<BsonDocument>("accounts");
+
+    // collection.find({});
+    debug.log(collection.find({}));
+   }
+
+    Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
