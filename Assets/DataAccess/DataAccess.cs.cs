cd9e43ed3c49a13d42f079b232a02af55172d5ac
Xenon Harpe <759005@ACDM.DS.SAIT.CA>
2020-11-09 20:22:33 -0700

    Adding file to shut git up

--- Assets/DataAccess/DataAccess.cs.cs
+++ Assets/DataAccess/DataAccess.cs.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 public class DataAccess.cs : MonoBehaviour
 {
     private const string MONGO_URI = "mongodb://username:password@127.0.0.1:27017";
-private const string DATABASE_NAME = "testDatabase";
-private MongoClient client;
-private IMongoDatabase db;
-
-client = new MongoClient(MONGO_URI);
-db = client.GetDatabase(DATABASE_NAME);
+    private const string DATABASE_NAME = "testDatabase";
+    private MongoClient client;
+    private IMongoDatabase db;
+    
+    client = new MongoClient(MONGO_URI);
+    db = client.GetDatabase(DATABASE_NAME);
     // Start is called before the first frame update
     void Start()
     {
