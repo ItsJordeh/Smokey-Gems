c6ccf10ee4aca36b674e6f9fb00ba300d6afb771
CJDeLeon25 <christjullian25@gmail.com>
2020-12-29 18:39:22 -0700

    added inventory canvas and potato
    added inventory canvas that turn on/off by pressing the B key. Added potato that follows the mouse when left clicking (for testing)

---
+++ Assets/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool isOpen = false;
+    public GameObject obj;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B)) 
+        {
+            if (isOpen)
+            {
+                Close();
+            }
+            else if(!isOpen)
+            {
+                Open();
+            }
+        }
+    }
+
+    private void Close()
+    {
+        obj.SetActive(false);
+        isOpen = false;
+
+    }
+    private void Open()
+    {
+        obj.SetActive(true);
+        isOpen = true;
+
+    }
+}
